f0940f1a0a5726de47fd3f49e1054b3d2daee682
prave <prave@PRAVEEN>
2020-12-24 23:34:03 +0530

    myfrstcmt

---
+++ src/main/java/mypack/sel.java
@@ -0,0 +1,16 @@
+package mypack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class sel {
+
+	public static void main(String[] args) {
+
+		System.out.println("hi maven");
+		
+		
+
+	}
+
+}
